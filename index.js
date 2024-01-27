e5e6c9f8c871e69d2307280265fea85a41b16e16
tinmaxwell <tinmaxwell@gmail.com>
2024-01-27 13:56:01 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()));
+  }
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
